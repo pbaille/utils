56ebf0606077725271809cf2566e630943f9f96d
pbaille <pierrebaille@gmail.com>
2016-01-12 08:31:44 +0100

    work

--- project.clj
+++ project.clj
@@ -5,9 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [backtick "0.3.3"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
-                 [com.gfredericks/debug-repl "0.0.7"]]
-  :profiles {:user {:dependencies [[com.gfredericks/debug-repl "0.0.6"]]
-                    :plugins      [[com.gfredericks/nrepl-53-monkeypatch "0.1.0"]]
-                    :repl-options {:nrepl-middleware
-                                   [com.gfredericks.debug-repl/wrap-debug-repl]}}})
+                 [org.clojure/math.numeric-tower "0.0.4"]])
--- src/utils/debug.clj
+++ src/utils/debug.clj
@@ -1,20 +1,22 @@
 (ns utils.debug
-  (:require [com.gfredericks.debug-repl :refer [break! unbreak! unbreak!! current-locals]]))
+  (:require #_[com.gfredericks.debug-repl :refer [break! unbreak! unbreak!! current-locals]]))
 
 (defn throw* [message] (throw (Exception. message)))
 
 (defmacro succeed-or [s f]
   `(try ~s (catch Exception e# ~f)))
 
-(defmacro !b [] `(break!))
-(defmacro !u [] `(unbreak!))
-(defmacro !!u [] `(unbreak!!))
+(comment
+  "debug repl shortcuts"
+  (defmacro !b [] `(break!))
+  (defmacro !u [] `(unbreak!))
+  (defmacro !!u [] `(unbreak!!))
 
-(defmacro or-dr
-  "try an expr and launch debug-repl if Exception"
-  [expr]
-  (let [line (:line (meta &form))
-        file *file*]
-    `(try ~expr (catch Exception _ (break! (str "file:" ~file "::" ~line))))))
+  (defmacro or-dr
+    "try an expr and launch debug-repl if Exception"
+    [expr]
+    (let [line (:line (meta &form))
+          file *file*]
+      `(try ~expr (catch Exception _ (break! (str "file:" ~file "::" ~line)))))))
 
 
